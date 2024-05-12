9ac16e5cf22c5983a2a77bce0e98545d6978ba4c
fsayguhij <js1peedee97@hotmail.com>
2024-05-13 06:42:30 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function isValidBST(root) {
-  const stack = [];
-  let inorder = -Infinity;
-  while (stack.length || root) {
-    while (root) {
-      stack.push(root);
-      root = root.left;
-    }
-    root = stack.pop();
-    if (root.val <= inorder) return false;
-    inorder = root.val;
-    root = root.right;
-  }
-  return true;
-}
+const flatten = (arr) => arr.reduce((a, v) => a.concat(v), []);
